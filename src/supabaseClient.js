93572f0cf6c517155d976e76759ceeb53d73a397
Ami Lanidor <amilanidor@gmail.com>
2026-05-28 22:27:49 +0300

    fix supabase key and loading

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL  = 'https://jeaizwuqxclvayfdbtcn.supabase.co'
-const SUPABASE_ANON = 'sb_publishable_VB7UkCFK2q2yXdBIEoKKXA_VVmdo2ax'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplYWl6d3VxeGNsdmF5ZmRidGNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTE3ODYsImV4cCI6MjA5NTM4Nzc4Nn0.Vtpq8pZ5o1SgIaaKVTtTRUgsu3hyIRQHYUccT8rl35c'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
